b20eb6b19862185d99ad9d9512c859071c1a6b4e
CRIDIP-SWD <mmockelyn@cridip.com>
2015-12-14 14:52:59 +0100

    GAMESHOP ADMIN UPDATE

--- index.php
+++ index.php
@@ -1,7 +1,17 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: CRIDIP-SWD
- * Date: 14/12/2015
- * Time: 14:45
- */
+require "app/autoloader.php";
+use App\Autoloader;
+Autoloader::register();
+
+if(isset($_GET['view']))
+{
+    $view = $_GET['view'];
+}else{
+    $view = 'dashboard';
+}
+
+ob_start();
+if($view === 'dashboard'){require 'view/dashboard.php';}
+
+$content = ob_get_clean();
+require 'view/template/default.php';
